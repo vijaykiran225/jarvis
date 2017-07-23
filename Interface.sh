b8db7101e76e37355dddbcb58afea5a739720857
Vijay Kiran <vijaykiran225@gmail.com>
2017-07-23 23:47:54 +0530

    Organize code

--- Interface.sh
+++ Interface.sh
@@ -1,2 +1,10 @@
-#todo
+echo "cleaning old binaries"
+rm -rf bin
+echo "done.."
+mkdir -p bin/java
+echo "Compiling code"
+javac -d ./bin/java  src/JavaInterface.java
+echo "done.."
+cd bin/java
+echo "Running"
 java JavaInterface
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jarvis
 Helper for Vijay with his work
+
+run the Interface.sh for triggering basic first cut flow of J
