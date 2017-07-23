ae141bb0e70f2b40720dc87e026c889f0a910eab
Vijay Kiran <vijaykiran225@gmail.com>
2017-07-23 23:08:31 +0530

    Adding Interfaces Java for code (for now) Sh to run

---
+++ Interface.sh
@@ -0,0 +1,2 @@
+#todo
+java JavaInterface
---
+++ JavaInterface.java
@@ -0,0 +1,7 @@
+public class JavaInterface  {
+	public static void main(String[] args) {
+		System.out.println("Jarvis Initializing");	
+		System.out.println("Jarvis processing");	
+		System.out.println("Jarvis signing off");	
+	}
+}
